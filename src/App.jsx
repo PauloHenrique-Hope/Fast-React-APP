76fc2fdb4a1781f7c904962318146b2823f93cfb
unknown <paulo.henriqq8@gmail.com>
2025-01-29 15:55:20 -0400

    Add CreateBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -4,7 +4,7 @@ import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from "../../utils/helpers";
+} from "../../helpers/helpers";
 
 const order = {
   id: "ABCDEF",
